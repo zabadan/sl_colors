9fc4b0d356450d08384ff8f3282f6b599d3e0a66
zabadan <repenkos@gmai.com>
2016-05-23 18:25:40 +0300

    +

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,20 @@ $(document).ready(function(){
 	$('#amount-range').on('propertychange input', f_chanhaRange);
 	$(':input').on('propertychange input', f_setStyleMode);
 	$('#mode_color').on('change', f_setStyleMode);
+
+	$('#copy_text').on('click', f_copyTextToClopboard)
+
+	// var button = document.getElementById('copy_text');
+	// button.addEventListener('click', function (e) {
+	// 	e.preventDefault();
+	// 	document.execCommand('copy', false, document.getElementById('textarea').select());
+	// });
+
 });
+
+function f_copyTextToClopboard() {
+ 	document.execCommand('copy', false, document.getElementById('textarea').select());
+}
 
 function f_chanhaRange() {
 	$('#amount_num').attr( 'value', $('#amount-range').val() );
@@ -14,13 +27,10 @@ function f_chanhaRange() {
 }
 
 
-function update(picker){
-	  	
-	    document.getElementById('output_primary_color').style.backgroundColor = 'rgb(' + Math.floor(picker.rgb[0]) + ',' + Math.floor(picker.rgb[1]) + ',' + Math.floor(picker.rgb[2]) + ')';
-	    
-	    primary_color_hsl = rgbToHsl( Math.floor(picker.rgb[0]) , Math.floor(picker.rgb[1]), Math.floor(picker.rgb[2]) );
-
-	    f_setStyleMode()
+function update(picker){	
+	document.getElementById('output_primary_color').style.backgroundColor = 'rgb(' + Math.floor(picker.rgb[0]) + ',' + Math.floor(picker.rgb[1]) + ',' + Math.floor(picker.rgb[2]) + ')';
+	primary_color_hsl = rgbToHsl( Math.floor(picker.rgb[0]) , Math.floor(picker.rgb[1]), Math.floor(picker.rgb[2]) );
+	f_setStyleMode()
 }
 
 function f_setStyleMode() {
@@ -35,43 +45,19 @@ function f_setDeliveryColor() {
 		name_delivery_color = $('#name_delivery_color').val(),
 		amount_num = $('#amount_num').val(),
 		derivate_color = $('#derivate_color').val(),
+		
 		primary_color_txt = '$'+ name_primary_color +': ' + primary_color + ';',
-		delivery_color_txt = '$'+ name_delivery_color +': ' + mode_txt + '( $' + name_primary_color + ', ' + amount_num + ' );';
+		delivery_color_txt = '$'+ name_delivery_color +': ' + mode_txt + '( $' + name_primary_color + ', ' + amount_num + ' );',
 
-    derivate_color_h = primary_color_hsl[0];
-    derivate_color_s = primary_color_hsl[1];
-    derivate_color_l = mode ? +(amount_num) + +(primary_color_hsl[2])  : primary_color_hsl[2] - amount_num;
+		derivate_color_h = primary_color_hsl[0];
+		derivate_color_s = primary_color_hsl[1];
+		derivate_color_l = mode ? +(amount_num) + +(primary_color_hsl[2])  : primary_color_hsl[2] - amount_num;
 
-	    document.getElementById('output_derivate_color').style.backgroundColor = 'hsl(' + derivate_color_h + ',' + derivate_color_s + '% ,' + derivate_color_l + '% )';
+	document.getElementById('output_derivate_color').style.backgroundColor = 'hsl(' + derivate_color_h + ',' + derivate_color_s + '% ,' + derivate_color_l + '% )';
 	$('#textarea').html(primary_color_txt + '\n'+ delivery_color_txt + '\nderivate_color_l = ' + derivate_color_l);
 
 }
 
-
-function hslToRgb(h, s, l){
-    var r, g, b;
-
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
 
 function rgbToHsl(r, g, b){
     r /= 255, g /= 255, b /= 255;
