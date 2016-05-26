4a973a3350632d75cbe88259932cbedb79044391
zabadan <repenkos@gmai.com>
2016-05-26 15:03:17 +0300

    reset works

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	dateG = {
 		mode: 0,
 		mode_txt: 'Darken',
-		delivery_color_hsl: [],
+		derivarive_color_hsl: [],
 		primary_color_hsl: [0, 0, 100],
 		globalMode: 0
 	};
@@ -10,12 +10,41 @@ $(document).ready(function(){
 	$('#amount-range').on('propertychange input', f_chanhaRange);
 	$(':input').on('propertychange input', f_setStyleMode);
 	
-	//$('#derivate_color').on('propertychange input', f_setStyleMode);
 	$('#mode_color').on('change', f_setStyleMode);
 
 	$('#copy_text').on('click', f_copyTextToClopboard);
+	$('#reset').on('click', f_resetDate);
 
 });
+
+function f_resetDate() {
+	dateG.mode = 0;
+	dateG.mode_txt = 'Darken';
+	dateG.derivarive_color_hsl = [0, 0, 100];
+	dateG.primary_color_hsl = [0, 0, 100];
+	dateG.globalMode = 0;
+	$('#primary_color').val( '#FFFFFF' );
+	document.getElementById("primary_color").style.backgroundColor="white";
+
+	$('#name_primary_color').val("primary_color") ;
+	$('#name_derivarive_color').val("derivarive_color") ;
+	
+	$('#mode_color').removeAttr('checked');
+	$('#amount-range').val(50);
+	$('#amount_num').val(50);
+
+
+	$('#derivate_color').val( '#FFFFFF' );
+	document.getElementById("derivate_color").style.backgroundColor="white";
+
+	$('#outputMode').val('no mode');
+	$('#outputAmount').val(' ');
+
+	document.getElementById("output_primary_color").style.backgroundColor="white";
+	document.getElementById("output_derivate_color").style.backgroundColor="white";
+
+	$('#textarea').val(' ');
+}
 
 function f_copyTextToClopboard() {
  	document.execCommand('copy', false, document.getElementById('textarea').select());
@@ -32,7 +61,7 @@ function update(picker, numblock){
 	//debugger;
 	if (dateG.globalMode) {
 		document.getElementById('output_derivate_color').style.backgroundColor = 'rgb(' + Math.floor(picker.rgb[0]) + ',' + Math.floor(picker.rgb[1]) + ',' + Math.floor(picker.rgb[2]) + ')';
-		dateG.delivery_color_hsl = rgbToHsl( Math.floor(picker.rgb[0]) , Math.floor(picker.rgb[1]), Math.floor(picker.rgb[2]) );
+		dateG.derivarive_color_hsl = rgbToHsl( Math.floor(picker.rgb[0]) , Math.floor(picker.rgb[1]), Math.floor(picker.rgb[2]) );
 		console.log('rgb(' + Math.floor(picker.rgb[0]) + ',' + Math.floor(picker.rgb[1]) + ',' + Math.floor(picker.rgb[2]) + ')')
 	} else {
 		document.getElementById('output_primary_color').style.backgroundColor = 'rgb(' + Math.floor(picker.rgb[0]) + ',' + Math.floor(picker.rgb[1]) + ',' + Math.floor(picker.rgb[2]) + ')';
@@ -40,7 +69,7 @@ function update(picker, numblock){
 	};
 	
 	console.log('dateG.primary_color_hsl = ' + dateG.primary_color_hsl);
-	console.log('dateG.delivery_color_hsl = ' + dateG.delivery_color_hsl);
+	console.log('dateG.derivarive_color_hsl = ' + dateG.derivarive_color_hsl);
 	console.log('dateG.globalMode = ' + dateG.globalMode);
 	f_setStyleMode()
 }
@@ -48,22 +77,22 @@ function update(picker, numblock){
 function f_setStyleMode() {
 	dateG.mode = document.querySelector('#mode_color').checked ? 1 : 0;
 	dateG.mode_txt = dateG.mode ? 'Lighten' : 'Darken';
-	f_setDeliveryColor();
+	f_setderivariveColor();
 }
 
-function f_setDeliveryColor() {
+function f_setderivariveColor() {
 	var primary_color = $('#primary_color').val(),
 		name_primary_color = $('#name_primary_color').val(),
-		name_delivery_color = $('#name_delivery_color').val(),
+		name_derivarive_color = $('#name_derivarive_color').val(),
 		amount_num = $('#amount_num').val(),
 		derivate_color = $('#derivate_color').val(),
 		primary_color_txt = '$'+ name_primary_color +': ' + primary_color + ';';
 		
 		if (dateG.globalMode) { /*--------find amount-------------*/
-			if(dateG.primary_color_hsl[0] == dateG.delivery_color_hsl[0] && dateG.primary_color_hsl[1] == dateG.delivery_color_hsl[1]){
+			if(dateG.primary_color_hsl[0] == dateG.derivarive_color_hsl[0] && dateG.primary_color_hsl[1] == dateG.derivarive_color_hsl[1]){
 				console.log('related colors!');
 
-				var differenceAmount = dateG.primary_color_hsl[2] - dateG.delivery_color_hsl[2]
+				var differenceAmount = dateG.primary_color_hsl[2] - dateG.derivarive_color_hsl[2]
 
 				if(differenceAmount > 0 ) {
 					dateG.mode_txt = 'Darken';
@@ -74,13 +103,13 @@ function f_setDeliveryColor() {
 				$('#outputMode').attr( 'value', dateG.mode_txt );
 				$('#outputAmount').attr( 'value', differenceAmount );
 
-				var delivery_color_txt = '$'+ name_delivery_color +': ' + dateG.mode_txt + '( $' + name_primary_color + ', ' + differenceAmount + ' );';
+				var derivarive_color_txt = '$'+ name_derivarive_color +': ' + dateG.mode_txt + '( $' + name_primary_color + ', ' + differenceAmount + ' );';
 			} else{
-				var delivery_color_txt = '$'+ name_delivery_color +': ' + derivate_color + ' ;';
+				var derivarive_color_txt = '$'+ name_derivarive_color +': ' + derivate_color + ' ;';
 			};
 		} else { /*----------find color------------*/
 			
-			var delivery_color_txt = '$'+ name_delivery_color +': ' + dateG.mode_txt + '( $' + name_primary_color + ', ' + amount_num + ' );';
+			var derivarive_color_txt = '$'+ name_derivarive_color +': ' + dateG.mode_txt + '( $' + name_primary_color + ', ' + amount_num + ' );';
 
 			derivate_color_h = dateG.primary_color_hsl[0];
 			derivate_color_s = dateG.primary_color_hsl[1];
@@ -91,7 +120,7 @@ function f_setDeliveryColor() {
 
 		
 	//document.getElementById('output_derivate_color').style.backgroundColor = 'hsl(' + derivate_color_h + ',' + derivate_color_s + '% ,' + derivate_color_l + '% )';
-	$('#textarea').html(primary_color_txt + '\n'+ delivery_color_txt);
+	$('#textarea').html(primary_color_txt + '\n'+ derivarive_color_txt);
 
 }
 
